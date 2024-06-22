f148d6884b20054f53ea8364388b74cf4673d29a
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-06-23 02:19:18 +0700

    Create japro.py

---
+++ japro.py
@@ -0,0 +1,21 @@
+import time
+
+import requests
+
+# List of URLs
+urls = [
+    "https://str.skom.id/vid/vidh/tv.php?id=17252",
+    ]
+
+# Visit each URL
+for url in urls:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Visited {url} - Status code: {response.status_code} - Berhasil")
+        else:
+            print(f"Visited {url} - Status code: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"Error visiting {url} - Error: {e}")
+    
+    time.sleep(30)  # Delay 30 detik
